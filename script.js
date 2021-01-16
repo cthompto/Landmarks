fc1c2e92334bb89f7f9b97af06ef85745aef16f9
Glitch (hello-webpage) <none>
2021-01-16 21:17:06 +0000

    ⛺️📊 Checkpoint ./index.html:12827033/1071 ./style.css:12827033/618 ./script.js:12827033/1981

--- script.js
+++ script.js
@@ -3,3 +3,40 @@
 
 // prints "hi" in the browser's dev tools console
 console.log("hi");
+
+const MODEL_URL = 'https://rawgit.com/justadudewhohacks/face-api.js/master/weights'
+
+const video = document.getElementById("video")
+
+Promise.all([
+  faceapi.nets.tinyFaceDetector.loadFromUri(MODEL_URL),
+  faceapi.nets.faceLandmark68Net.loadFromUri(MODEL_URL),
+  faceapi.nets.faceRecognitionNet.loadFromUri(MODEL_URL),
+  faceapi.nets.faceExpressionNet.loadFromUri(MODEL_URL)
+]).then(startVideo)
+
+function startVideo() {
+  navigator.getUserMedia({ video: {} },
+    stream => video.srcObject = stream,
+    err => console.error(err)
+)
+}
+
+video.addEventListener("play", () => {
+  const canvas = faceapi.createCanvasFromMedia(video)
+  var item = document.getElementById("center")
+  item.replaceChild(canvas, item.firstChild)
+  const displaySize = { width: video.width, height: video.height }
+  faceapi.matchDimensions(canvas, displaySize)
+  setInterval(async() => {
+     const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
+     const resizedDetections = faceapi.resizeResults(detections, displaySize)
+
+     //faceapi.draw.drawDetections(canvas, resizedDetections)
+     faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+     //faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
+   }, 400)
+   setInterval(async() => {
+     canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height)
+   }, 15000)
+})
