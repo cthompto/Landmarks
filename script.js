8ad3b191bce1062d96c3ab89591c1ed5a20d92b1
Glitch (hello-webpage) <none>
2021-01-22 07:33:39 +0000

    👱🛣 Checkpoint ./index.html:12827033/46 ./style.css:12827033/4 ./script.js:12827033/2

--- script.js
+++ script.js
@@ -52,7 +52,7 @@ video.addEventListener("play", () => {
     const resizedDetections = faceapi.resizeResults(detections, displaySize)
     canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height)
     //faceapi.draw.drawDetections(canvas, resizedDetections)
-    //faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
     faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
     resizedDetections.forEach( detection => {
       const box = detection.detection.box
