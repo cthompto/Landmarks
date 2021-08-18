3dc2d756d8fa84cc91a19e84e26892df6ab7e482
Glitch (landmarks) <41555599+cthompto@users.noreply.github.com>
2021-08-17 17:10:41 -0700

    update icon links
    update icon links and test random image for rote

---
+++ views/src/pic.js
@@ -0,0 +1,7 @@
+window.onload = choosePic;
+
+var myPix = new Array("assets/rote1.jpg","assets/rote2.jpg","assets/rote3.jpg","assets/rote4.jpg","assets/rote5.jpg","assets/rote6.jpg","assets/rote7.jpg","assets/rote8.jpg","assets/rote9.jpg");
+
+function choosePic() {
+     var randomNum = Math.floor(Math.random() * myPix.length);
+     document.getElementById("bgImg").src = myPix[randomNum];
